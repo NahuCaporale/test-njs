447a7634e1ad1d25a379fc108c317681ef3b26c6
nahu <nahucaporale21@gmail.com>
2026-05-02 20:44:03 -0300

    de-bug

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,3 @@
 import { Inter, Lusitana } from "next/font/google";
 export const inter = Inter({ subsets: ["latin"] });
-export const lusi = Lusitana({ subsets: ["latin"], weight: "400" });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
